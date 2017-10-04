d72c625d24b582454e9569cc1f1ee8f40dbfbbc9
Cesar Rodriguez <cesar_rodriguez@troweprice.com>
2017-10-03 22:29:53 -0400

    removes full version

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# docker-hub-images
-Automatic builds of container images for Docker Hub
+# Docker terraform providers
+### Usage
+This repository automatically builds containers for [`terraform`](https://terraform.io) including common providers.
 
-terraform (full) [![](https://badge.imagelayers.io/hashicorp/terraform:latest.svg)](https://imagelayers.io/?images=hashicorp/terraform:latest 'Get your own badge on imagelayers.io')
+##### terraform Docker Container
 
-terraform (light)
-[![](https://badge.imagelayers.io/hashicorp/terraform:light.svg)](https://imagelayers.io/?images=hashicorp/terraform:light 'Get your own badge on imagelayers.io')
+This container will copy the current stable version of the terraform binary into the container, and set it for use as the default entrypoint.
+
+You can use it as follows:
+```shell
+docker run -i -t cesar-rodriguez/terraform <command>
+```
+
+For example:
+Initialize the plugins (You will only need to do this once)
+
+```shell
+docker run -i -t -v $(pwd):/app/ -w /app/ cesar-rodriguez/terraform init
+```
+
+
+```shell
+docker run -i -t -v $(pwd):/app/ -w /app/ cesar-rodriguez/terraform plan
+```
--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -11,23 +11,19 @@ if [[ $# -ne 1 ]]; then
 fi
 
 version=$1
-dockerfile_version=$(grep TERRAFORM_VERSION= ${base}/Dockerfile-light | cut -d= -f2)
+dockerfile_version=$(grep TERRAFORM_VERSION= ${base}/Dockerfile | cut -d= -f2)
 
 if [[ $version != $dockerfile_version ]]; then
-    echo "Version mismatch in 'Dockerfile-light'"
+    echo "Version mismatch in 'Dockerfile'"
     echo "found ${dockerfile_version}, expected ${version}."
     echo "Make sure the versions are correct."
     exit 1
 fi
 
 echo "Building docker images for terraform ${version}..."
-docker build -f "${base}/Dockerfile-full" -t hashicorp/terraform:full .
-docker build -f "${base}/Dockerfile-light" -t hashicorp/terraform:light .
-docker tag hashicorp/terraform:light hashicorp/terraform:${version}
-docker tag hashicorp/terraform:light hashicorp/terraform:latest
+docker build -f "${base}/Dockerfile" -t therasec/terraform-providers:latest .
+docker tag therasec/terraform-providers:latest therasec/terraform-providers:${version}
 
 echo "Uploading docker images for terraform ${version}..."
-docker push hashicorp/terraform:${version}
-docker push hashicorp/terraform:latest
-docker push hashicorp/terraform:light
-docker push hashicorp/terraform:full
+docker push therasec/terraform-providers:${version}
+docker push therasec/terraform-providers:latest
